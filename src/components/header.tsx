3fd02f678ee0856965ab9ae33be3245a3dc2728f
Breno-Mateus <brenomateus36@gmail.com>
2026-01-21 17:00:51 -0300

    feat: add prop

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,17 @@
 import { FiPrinter } from "react-icons/fi";
 
-const Header = () => {
+interface HeaderProps {
+    onExport: () => void;
+}
+
+const Header = ({ onExport }: HeaderProps) => {
     return ( 
-        <header className="flex justify-between p-4">
+        <header className="flex justify-between items-center p-4 border-b border-smooth bg-primary">
             <h1 className="text-2xl font-bold">Relatório de Vendas</h1>
-            <button className="bg-brand rounded-md py-1 px-2 text-primary font-medium flex items-center gap-2 hover:cursor-pointer hover:opacity-80 transition-opacity">
+            <button 
+                className="bg-brand rounded-md py-1 px-2 text-primary font-medium flex items-center gap-2 hover:cursor-pointer hover:opacity-80 transition-opacity"
+                onClick={onExport}
+            >
                 <FiPrinter/> Exportar PDF
             </button>
         </header>
